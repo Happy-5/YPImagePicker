132c3d19ff1aa2147f388d110420795fe8b77885
Yehezkiel Litbagay <yehezkiel@happy5.co>
2019-10-23 00:33:00 +0700

    Fix color

--- Source/Pages/Gallery/YPAssetViewContainer.swift
+++ Source/Pages/Gallery/YPAssetViewContainer.swift
@@ -81,8 +81,7 @@ class YPAssetViewContainer: UIView {
         multipleSelectionButton-15-|
         multipleSelectionButton.setImage(YPConfig.icons.multipleSelectionOffIcon, for: .normal)
         multipleSelectionButton.Bottom == zoomableView!.Bottom - 15
-		multipleSelectionButton.clipsToBounds = true
-        multipleSelectionButton.layer.cornerRadius = multipleSelectionButton.frame.height / 2
+        
     }
     
     // MARK: - Square button
@@ -118,8 +117,7 @@ class YPAssetViewContainer: UIView {
         let image = on ? YPConfig.icons.multipleSelectionOffIcon.withRenderingMode(.alwaysTemplate) : YPConfig.icons.multipleSelectionOffIcon.withRenderingMode(.alwaysOriginal)
 		multipleSelectionButton.setImage(image, for: .normal)
 		if on {
-			multipleSelectionButton.tintColor = UIColor.clear
-			multipleSelectionButton.backgroundColor = UIColor(red: 252.0 / 255.0, green: 60.0 / 255.0, blue: 98.0 / 255.0, alpha: 1.0)
+			multipleSelectionButton.tintColor = UIColor.init(red: 252.0 / 255.0, green: 60.0 / 255.0, blue: 98.0 / 255.0, alpha: 1.0)
 		}
 		
 		refreshSquareCropButton()
