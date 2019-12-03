91c6cd84b91cd82643352b3ac787ba4dad09870d
Stevanus Prasetyo Soemadi <stevanus.pras@gmail.com>
2019-12-03 17:35:08 +0700

    trial

--- Source/Pages/Gallery/YPLibraryVC.swift
+++ Source/Pages/Gallery/YPLibraryVC.swift
@@ -153,9 +153,6 @@ public class YPLibraryVC: UIViewController, YPPermissionCheckable {
 
         if multipleSelectionEnabled {
             if selection.isEmpty {
-				
-				let currentAsset = mediaManager.fetchResult[currentlySelectedIndex]
-				self.currentlyIndex = mediaManager.fetchResult.index(of: currentAsset)
 				refreshMediaRequest()
 
                 selection = [
@@ -255,6 +252,8 @@ public class YPLibraryVC: UIViewController, YPPermissionCheckable {
         if mediaManager.fetchResult.count > 0 {
 			
 			if multipleSelectionEnabled {
+				let currentAsset = mediaManager.fetchResult[currentlySelectedIndex]
+				self.currentlyIndex = mediaManager.fetchResult.index(of: currentAsset)
 				changeAsset(mediaManager.fetchResult[currentlyIndex])
 				v.collectionView.reloadData()
 				v.collectionView.selectItem(at: IndexPath(row: currentlyIndex, section: 0),
