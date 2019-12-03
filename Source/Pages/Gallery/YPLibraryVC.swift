3591930792e02a7f64778b92000cee2841855c55
Stevanus Prasetyo Soemadi <stevanus.pras@gmail.com>
2019-12-03 19:00:17 +0700

    back to default

--- Source/Pages/Gallery/YPLibraryVC.swift
+++ Source/Pages/Gallery/YPLibraryVC.swift
@@ -154,8 +154,8 @@ public class YPLibraryVC: UIViewController, YPPermissionCheckable {
         if multipleSelectionEnabled {
             if selection.isEmpty {
 				let currentAsset = mediaManager.fetchResult[currentlySelectedIndex]
-				self.currentlyIndex = mediaManager.fetchResult.index(of: currentAsset)
 				refreshMediaRequest()
+				self.currentlyIndex = mediaManager.fetchResult.index(of: currentAsset)
 
                 selection = [
                     YPLibrarySelection(index: self.currentlyIndex,
@@ -254,20 +254,11 @@ public class YPLibraryVC: UIViewController, YPPermissionCheckable {
         if mediaManager.fetchResult.count > 0 {
 			
 			if multipleSelectionEnabled {
-				v.collectionView.reloadData()
-				let currentAsset = mediaManager.fetchResult[currentlySelectedIndex]
-				self.currentlyIndex = mediaManager.fetchResult.index(of: currentAsset)
-				
 				changeAsset(mediaManager.fetchResult[currentlyIndex])
-
+				v.collectionView.reloadData()
 				v.collectionView.selectItem(at: IndexPath(row: currentlyIndex, section: 0),
 											animated: false,
 											scrollPosition: [])
-				
-				
-				print("test1", currentlySelectedIndex)
-				print("test2", currentlyIndex)
-
 			} else {
 				changeAsset(mediaManager.fetchResult[currentlySelectedIndex])
 				v.collectionView.reloadData()
